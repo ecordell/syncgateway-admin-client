014402b95e6ec4d02083cdd03f96cb4bda8dc56c
Evan Cordell <cordell.evan@gmail.com>
2015-12-11 17:45:09 -0600

    Bump version: 0.1.0 → 0.1.1

--- syncgateway/__init__.py
+++ syncgateway/__init__.py
@@ -2,6 +2,6 @@ __author__ = 'Evan Cordell'
 __copyright__ = 'Copyright 2012-2015 Localmed, Inc.'
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __version_info__ = tuple(__version__.split('.'))
 __short_version__ = __version__
